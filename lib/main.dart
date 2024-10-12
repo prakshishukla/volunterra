97f8cd11e5bbed02a0797936696401922e805f55
nimaib2 <nimaib@uw.edu>
2024-10-12 15:47:49 -0700

    imported maps and stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+// File: lib/main.dart
+
+import 'dart:html';
+import 'package:volunterra/google_maps_wrapper.dart';
 
 void main() {
+  final script = ScriptElement()
+    ..type = 'text/javascript'
+    ..src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBvMc1kAu_VOHZdq1xXg698rWuv0h5DDz4';
+  
+  document.head!.appendChild(script);
+
+  script.onLoad.listen((_) {
+    final mapOptions = MapOptions(
+      zoom: 8,
+      center: LatLng(37.5, -122.0),
+    );
+
+    final mapElement = document.getElementById('map');
+    Map(mapElement!, mapOptions);
+  });
+
   runApp(const MyApp());
 }
 
